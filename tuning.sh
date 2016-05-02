13c16d7ea16bcac900596bb5b3d7f9518fb51452
ruilinchu <ruilinchu@gmail.com>
2016-05-02 14:14:33 -0700

    add

--- tuning.sh
+++ tuning.sh
@@ -1,15 +1,15 @@
 
 #meta
-echo deadline > /sys/block/sdX/queue/scheduler
-echo 128 > /sys/block/sdX/queue/nr_requests
+echo deadline > /sys/block/sda7/queue/scheduler
+echo 128 > /sys/block/sda7/queue/nr_requests
 
 echo madvise > /sys/kernel/mm/redhat_transparent_hugepage/enabled
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor >/dev/null
 
 #data
-echo deadline > /sys/block/sdX/queue/scheduler
-echo 4096 > /sys/block/sdX/queue/nr_requests
-echo 4096 > /sys/block/sdX/queue/read_ahead_kb
+echo deadline > /sys/block/sdb/queue/scheduler
+echo 4096 > /sys/block/sdb/queue/nr_requests
+echo 4096 > /sys/block/sdb/queue/read_ahead_kb
 echo 5 > /proc/sys/vm/dirty_background_ratio
 echo 10 > /proc/sys/vm/dirty_ratio
 echo 50 > /proc/sys/vm/vfs_cache_pressure
