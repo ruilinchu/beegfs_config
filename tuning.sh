13860987066b173c38cf1caf4f2ae8a5b142184b
ruilinchu <ruilinchu@gmail.com>
2016-10-18 11:10:50 -0700

    Update tuning.sh

--- tuning.sh
+++ tuning.sh
@@ -15,6 +15,6 @@ echo 4096 > /sys/block/sdb/queue/read_ahead_kb
 echo 5 > /proc/sys/vm/dirty_background_ratio
 echo 10 > /proc/sys/vm/dirty_ratio
 echo 50 > /proc/sys/vm/vfs_cache_pressure
-echo 262144 > /proc/sys/vm/min_free_kbytes
+echo 4194304 > /proc/sys/vm/min_free_kbytes
 echo always > /sys/kernel/mm/transparent_hugepage/enabled
 echo always > /sys/kernel/mm/transparent_hugepage/defrag
