479cc0a5fa5ff43fcfffd0010ef0316e7c9debb1
ruilinchu <ruilinchu@gmail.com>
2019-09-23 12:48:04 -0700

    Update create_pool.sh

--- create_pool.sh
+++ create_pool.sh
@@ -1,11 +1,12 @@
 #!/bin/sh                                                                                                                                                                                                                                                      
 declare -i pn=1;
 
-for i in 0; do
+for i in $(seq 0 4); do
  for j in $(seq 0 9) A B; do
      devices=$(grep " $i$j  /device" all_1 | cut -d"/" -f9 | xargs);
-     echo creating zfs pool $pn with devices: $devices;
-     pn=pn+1;
+     if [ ! -z "$devices" ];then
+         echo creating zfs pool $pn with devices: $devices;
+         pn=pn+1;
+     fi
  done;
 done
-
