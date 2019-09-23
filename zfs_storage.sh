99cfa3871966392a71dd2de84870dc6a0fb62974
ruilinchu <ruilinchu@gmail.com>
2019-09-23 16:30:23 -0700

    Update zfs_storage.sh

--- zfs_storage.sh
+++ zfs_storage.sh
@@ -1,5 +1,12 @@
 ##put in a start up systemd service
 
+##disable HyperThreading
+for i in $(grep -H . /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | sort -n -t ',' -k 2 -u | cut -d"," -f2); 
+do 
+  echo 0 > /sys/devices/system/cpu/cpu${i}/online;
+done
+
+#zfs tuning for storage
 echo 4194304 > /sys/module/zfs/parameters/zfs_max_recordsize
 echo deadline > /sys/module/zfs/parameters/zfs_vdev_scheduler
 echo 1310720 > /sys/module/zfs/parameters/zfs_read_chunk_size
