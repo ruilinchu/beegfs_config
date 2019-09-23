ccc9be22c5977f24a4592e4b2e817e43678ec8ad
ruilinchu <ruilinchu@gmail.com>
2019-09-23 13:23:36 -0700

    Update create_pool.sh

--- create_pool.sh
+++ create_pool.sh
@@ -5,7 +5,7 @@ for i in $(seq 0 4); do
  for j in $(seq 0 9) A B; do
      if [ $pn -lt 58 ];then
 	 devices=$(grep " $i$j  /device" all_2 | cut -d"/" -f9 | xargs);
-	 echo creating zfs pool $pn with devices: $devices;
+	 echo creating zfs pool $pn with command "zpool create -f -O compression=off -O recordsize=4M -o ashift=12 -O atime=off -O xattr=off -O dedup=off -m /data/pool$pn pool$pn raidz2 $devices"
 	 pn=pn+1;
      fi
  done;
