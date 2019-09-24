ff95064b9c132f5baaf59d2a78e86bf5a4f9346e
ruilinchu <ruilinchu@gmail.com>
2019-09-23 17:19:06 -0700

    Update create_pool.sh

--- create_pool.sh
+++ create_pool.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+
+for i in `lsblk | grep disk | grep "1.8T" | awk '{print $1}'`; do path=$(echo /sys/class/enclosure/*/*/device/block/$i);echo $path / wwn-0x$(cat "$path/../../wwid"|cut -d"." -f2); done > all
+
 declare -i pn=1;
 
 for i in $(seq 0 4); do
  for j in $(seq 0 9) A B; do
      if [ $pn -lt 58 ];then
-	 devices=$(grep " $i$j  /device" all_2 | cut -d"/" -f9 | xargs);
-	 echo creating zfs pool $pn with command "zpool create -f -O compression=off -O recordsize=4M -o ashift=12 -O atime=off -O xattr=off -O dedup=off -m /data/pool$pn pool$pn raidz2 $devices"
+	 devices=$(grep " $i$j  /device" all | cut -d"/" -f9 | xargs);
+	 if (( $pn % 2 )); then
+	     echo  "zpool create -f -O compression=off -O recordsize=4M -o ashift=12 -O atime=off -O xattr=off -O dedup=off -m /data/pool-$pn pool-$pn raidz2 $devices" >> command_1.sh;
+	 else
+	     echo  "zpool create -f -O compression=off -O recordsize=4M -o ashift=12 -O atime=off -O xattr=off -O dedup=off -m /data/pool-$pn pool-$pn raidz2 $devices" >> command_2.sh;
+	 fi
 	 pn=pn+1;
      fi
  done;
