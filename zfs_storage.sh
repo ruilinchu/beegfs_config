130f0904e2e471ecc0da11b2d3dd136f35ebc2ab
ruilinchu <ruilinchu@gmail.com>
2019-09-23 19:14:51 -0700

    Update zfs_storage.sh

--- zfs_storage.sh
+++ zfs_storage.sh
@@ -1,4 +1,5 @@
 ##put in a start up systemd service
+#/bin/bash
 
 ##disable HyperThreading
 for i in $(grep -H . /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | sort -n -t ',' -k 2 -u | cut -d"," -f2); 
@@ -12,3 +13,9 @@ echo deadline > /sys/module/zfs/parameters/zfs_vdev_scheduler
 echo 1310720 > /sys/module/zfs/parameters/zfs_read_chunk_size
 echo 0 > /sys/module/zfs/parameters/zfs_prefetch_disable
 echo 262144 > /sys/module/zfs/parameters/zfs_vdev_aggregation_limit
+
+declare -i pn=1
+while [ $pn -lt 58 ]; do
+  zpool import -d /dev/disk/by-id -N pool-$pn;
+  pn=pn+2;
+done
