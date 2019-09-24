7e50424b7b616a71e511c81ef7b27a8827f9c672
ruilinchu <ruilinchu@gmail.com>
2019-09-23 19:17:59 -0700

    Update zfs_storage.sh

--- zfs_storage.sh
+++ zfs_storage.sh
@@ -14,6 +14,8 @@ echo 1310720 > /sys/module/zfs/parameters/zfs_read_chunk_size
 echo 0 > /sys/module/zfs/parameters/zfs_prefetch_disable
 echo 262144 > /sys/module/zfs/parameters/zfs_vdev_aggregation_limit
 
+## disable zfs-import-cache, zfs-import-scan, zfs-mount, zfs-share services, do manual import, 
+## 1 import odd pools, 2 import even pools
 declare -i pn=1
 while [ $pn -lt 58 ]; do
   zpool import -d /dev/disk/by-id -N pool-$pn;
