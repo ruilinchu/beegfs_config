8dd803b7ba6e33c3b2d6cd134602805da79fb15a
ruilinchu <ruilinchu@gmail.com>
2019-12-06 17:12:24 -0800

    Update makepool.sh

--- makepool.sh
+++ makepool.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #set -x
 #
-# this is for a 60 tray 5rowx12col JBOD enclosure, 2col=1vdev, 3vdev=1pool=30HDD
+# this is for a 60 tray 5rowx12col JBOD enclosure, 2col=1vdev, 3vdev=1pool=30HDD, for enc 01 with 4TB hdd
+# for rest of 9 enc with 2TB hdd, 2col=1vdev, 6vdev=1pool
 #
 
 declare -a c
@@ -17,20 +18,22 @@ echo ${c[12]}
 echo ${d[11]}
 #2x multipath, using only c for pool creation, followed by export and zpool import -d /dev/mapper -a
 
-zpool create -f -O compression=lz4 -O recordsize=4M -o ashift=12 -O atime=off -O xattr=off -O dedup=off -m /data/pool-1 \
-    pool-1 raidz2 ${c[1]} ${c[2]} \
-    raidz2 ${c[3]} ${c[4]} \
-    raidz2 ${c[5]} ${c[6]}
+##pool-01 ~ pool-10
+poolname=pool-$(hostname -s| cut -c4-)
 
-zpool create -f -O compression=lz4 -O recordsize=4M -o ashift=12 -O atime=off -O xattr=off -O dedup=off -m /data/pool-2 \
-    pool-2 raidz2 ${c[7]} ${c[8]} \
+zpool create -f -O compression=lz4 -O recordsize=4M -o ashift=12 -O atime=off -O xattr=off -O dedup=off -m /data/$poolname \
+    $poolname \
+    raidz2 ${c[1]} ${c[2]} \
+    raidz2 ${c[3]} ${c[4]} \
+    raidz2 ${c[5]} ${c[6]} \
+    raidz2 ${c[7]} ${c[8]} \
     raidz2 ${c[9]} ${c[10]} \
-    raidz2 ${c[11]} ${c[12]}
+    raidz2 ${c[11]} ${c[12]} \
+    cache nvme0n1
 
 zpool create -f -O compression=lz4 -O atime=off -O xattr=sa -O dedup=off -O autotrim=on -m /data/meta meta mirror sdb sdc
 
-zpool export pool-1
-zpool export pool-2
+zpool export $poolname
 
-zpool import -d /dev/mapper pool-1
-zpool import -d /dev/mapper pool-2
+zpool import -d /dev/mapper $poolname
+
