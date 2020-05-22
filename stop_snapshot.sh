39bbcf7c7ce198ee85ce721d12be7c1ebe026d88
ruilinchu <ruilinchu@gmail.com>
2020-05-22 22:59:56 +0000

    add

--- stop_snapshot.sh
+++ stop_snapshot.sh
@@ -1,7 +1,7 @@
+systemctl stop beegfs-mgmtd@snapshot
 systemctl stop beegfs-meta@snapshot
 systemctl stop beegfs-storage@snapshot
-systemctl stop beegfs-mgmtd@snapshot
 
 zfs destroy -r meta/snapshot
-zfs destroy -r storage/snapshot
+zfs destroy -r data/snapshot
 zfs destroy -r mgmt/snapshot
