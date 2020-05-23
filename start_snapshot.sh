456a4726ddfaa2b8f3038c8c7e8ea2c7a4a9ad1f
ruilinchu <ruilinchu@gmail.com>
2020-05-23 05:00:22 +0000

    add

--- start_snapshot.sh
+++ start_snapshot.sh
@@ -1,7 +1,22 @@
-zfs clone data@zfs-auto-snap_hourly-2020-05-22-2000      data/snapshot
-zfs clone meta@zfs-auto-snap_hourly-2020-05-22-2000      meta/snapshot
-zfs clone mgmt@zfs-auto-snap_hourly-2020-05-22-2000      mgmt/snapshot
+#!/bin/bash
 
+mgmt_node=bee1
+meta_node=bee[1-2]
+storage_node=bee[1-2]
+
+snap_name=hourly-2020-05-22-2320
+
+pdsh -w ${mgmt_node} << EOF
+zfs clone -o mountpoint=/snapshot/mgmt mgmt@zfs-auto-snap_${snap_name}      mgmt/snapshot
 systemctl start beegfs-mgmtd@snapshot
+EOF
+
+pdsh -w ${meta_node} << EOF
+zfs clone -o mountpoint=/snapshot/meta meta@zfs-auto-snap_${snap_name}      meta/snapshot
 systemctl start beegfs-meta@snapshot
+EOF
+
+pdsh -w ${storage_node} << EOF
+zfs clone -o mountpoint=/snapshot/data data@zfs-auto-snap_${snap_name}      data/snapshot
 systemctl start beegfs-storage@snapshot
+EOF
